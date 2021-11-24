1985150eaf18ac4b09b2e65a8151b1e730608b4b
Senh Mo Chuang <senhmo@gmail.com>
2021-11-23 23:06:31 -0600

    Better Dacian Algo

--- README.md
+++ README.md
@@ -10,14 +10,33 @@ Use the built-in function using the compressed string from the ElectroDacus
 
 >`python dacian.py`
 
-### Disconnect Issues on Raspberry Pi:
+I highly recommend using virtual environments for a good Python practice
+
+### Disconnect Issues on Raspberry Pi UART Port:
 To connect to the device, you need to connect GPIO 14 to the SBMS0's top left pin on the 16 pin connector. You will also need to connect one of the Raspberry Pi's and one of the 3v3 Pin to SBMS0. The 3v3 pin on the SMBS0 is located on either pin on third row COUNT FROM THE BOTTOM on the 16 pin connector. The ground pin of the SMBS0 is located on either pin on the bottom row on the 16 pin connector. See:
 
 >https://pinout.xyz/pinout/uart
 
 Edit the kernel command line with sudo nano /boot/cmdline.txt. Find the console entry that refers to the serial0 device, and remove it, including the baud rate setting. It will look something like console=serial0,115200. Make sure the rest of the line remains the same, as errors in this configuration can stop the Raspberry Pi from booting
 
+
+
+# Other Troubleshooting Ideas:
 Be sure you enable UART on the SBMS0
+sudo vi /boot/cmdline.txt
+>console=tty1 root=PARTUUID=3c7b78f1-02 rootfstype=ext4 fsck.repair=yes rootwait
+sudo systemctl stop serial-getty@AMA0.service
+reboot
+enable serial in raspi-config no and then yes
+sudo usermod  -a -G tty pi
+sudo chmod 666 /dev/ttyAMA0
+
+Open /boot/config.txt file. sudo vi /boot/config.txt
+dtoverlay=disable-bt
+dtoverlay=pi3-disable-bt
+
+We also need to run to stop BT modem trying to use UART
+sudo systemctl disable hciuart
 
 It wasn't easy getting this to work in Python. If you enjoy my work, maybe you can buy me a cup of coffee?
 
--- dacian.py
+++ dacian.py
@@ -2,48 +2,64 @@ from datetime import datetime
 from pprint import pprint
 import serial
 
+def dacian_algo(structual_list):
+    # partial_sum = [each_digit*91**(idx) for idx, each_digit in enumerate(structual_list[::-1])]
+    # partial_sum = map(lambda idx, each_digit: each_digit*91**(idx), range(len(structual_list)), structual_list[::-1])
+    partial_sum = map(lambda each_digit_tuple: each_digit_tuple[1]*91**(each_digit_tuple[0]), enumerate(structual_list[::-1]))
+    return sum(partial_sum)
+
 def decoding_dacian_electrodacus_data(compressed_data):
     payload = {}
     uncompressed_list_char = [ord(each_char)-35 for each_char in compressed_data]
     single_char_value = uncompressed_list_char[:6]
     double_char_value = uncompressed_list_char[6:7+21]
     triple_char_value = uncompressed_list_char[7+22:]
-    double_list = [upper_d*91+lower_d for upper_d, lower_d in zip(double_char_value[::2], double_char_value[1::2])]
-    triple_list = [upper_t*91**2+mid_t*91+lower_t for upper_t, mid_t, lower_t in zip(triple_char_value[::3], triple_char_value[1::3], triple_char_value[2::3])]
+    # double_list = [upper_d*91+lower_d for upper_d, lower_d in zip(double_char_value[::2], double_char_value[1::2])]
+    double_list = [dacian_algo(each_pair) for each_pair in zip(double_char_value[::2], double_char_value[1::2])]
+    # triple_list = [upper_t*91**2+mid_t*91+lower_t for upper_t, mid_t, lower_t in zip(triple_char_value[::3], triple_char_value[1::3], triple_char_value[2::3])]
+    triple_list = [dacian_algo(each_pair) for each_pair in zip(triple_char_value[::3], triple_char_value[1::3], triple_char_value[2::3])]
     payload["Sample Time"] = datetime(year=2000+single_char_value[0], month=single_char_value[1], day=single_char_value[2], hour=single_char_value[3], minute=single_char_value[4], second=single_char_value[5]).strftime("%c")
     payload["SOC"] = double_list[0]
-    payload["Cell1"] = double_list[1]/1000
-    payload["Cell2"] = double_list[2]/1000
-    payload["Cell3"] = double_list[3]/1000
-    payload["Cell4"] = double_list[4]/1000
-    payload["Cell5"] = double_list[5]/1000
-    payload["Cell6"] = double_list[6]/1000
-    payload["Cell7"] = double_list[7]/1000
-    payload["Cell8"] = double_list[8]/1000
-    payload["Internal Temperature F"] = (double_list[9]-450)/10*9/5+32
-    payload["Internal Temperature C"] = (double_list[9]-450)/10
-    payload["External Temperature F"] = (double_list[10]-450)/10*9/5+32
-    payload["External Temperature C"] = (double_list[10]-450)/10
+    payload["Voltage of Cell1"] = double_list[1]/1000
+    payload["Voltage of Cell2"] = double_list[2]/1000
+    payload["Voltage of Cell3"] = double_list[3]/1000
+    payload["Voltage of Cell4"] = double_list[4]/1000
+    payload["Voltage of Cell5"] = double_list[5]/1000
+    payload["Voltage of Cell6"] = double_list[6]/1000
+    payload["Voltage of Cell7"] = double_list[7]/1000
+    payload["Voltage of Cell8"] = double_list[8]/1000
+    payload["Temperature Internal F"] = (double_list[9]-450)/10*9/5+32
+    payload["Temperature Internal C"] = (double_list[9]-450)/10
+    payload["Temperature External F"] = (double_list[10]-450)/10*9/5+32
+    payload["Temperature External C"] = (double_list[10]-450)/10
     battery_value = triple_list[0]/1000
-    payload["Current of Battery"] = battery_value if compressed_data[28] !="-" else -battery_value
-    payload["Current of PV1"] = triple_list[1]/1000
+    payload["Current of Battery (- discharging)"] = battery_value if compressed_data[28] !="-" else -battery_value
+    current_pv1 = triple_list[1]/1000
+    payload["Current of PV1"] = current_pv1
     payload["Current of PV2"] = triple_list[2]/1000
     payload["Current of Load"] = triple_list[3]/1000
     payload["ADC2"] = triple_list[4]
     payload["ADC3"] = triple_list[5]
     payload["ADC4"] = triple_list[6]
-    payload["Heat1"] = triple_list[7]
-    payload["Heat2"] = triple_list[8]
+    payload["Temperature of Heat1"] = triple_list[7]
+    payload["Temperature of Heat2"] = triple_list[8]
     error_codes_tuple = list(filter(lambda each_dig:  int(each_dig[0])==1, zip(bin(triple_list[9]).replace("0b","")[::-1], "OV OVLK UV UVLK IOT COC DOC DSC CELF OPEN LVC ECCF CFET EOC DFET".split(' '))))
     error_codes = [error[1] for error in error_codes_tuple]    
     payload["Error"] =  error_codes
-    payload["Voltage of Battery"] = sum(double_list[1:9])/1000
-    payload["Voltage Cell Delta"] = (max(double_list[1:9]) - min(double_list[1:9]))/1000
+    battery_voltage = sum(double_list[1:9])/1000
+    payload["Voltage of Battery"] = battery_voltage
+    volt_min = min(double_list[1:9])
+    volt_max = max(double_list[1:9])
+    payload["Voltage Cell Delta"] = (volt_max - volt_min)/1000
+    payload["Voltage of Min Cell"] = volt_min/1000
+    payload["Voltage of Max Cell"] = volt_max/1000
+    payload["Power PV1"] = current_pv1 * battery_voltage
     return payload
 
 while True:
-        ser = serial.Serial('/dev/ttyAMA0', 115200)
-        data = ser.readline()
-        compressed_string = data.decode('utf-8').strip()
-        uncompressed_string = decoding_dacian_electrodacus_data(compressed_string)
-        pprint(uncompressed_string)
+    ser = serial.Serial('/dev/ttyAMA0', 115200)
+    data = ser.readline()
+    compressed_string = data.decode('utf-8').strip()
+    print(compressed_string)
+    uncompressed_string = decoding_dacian_electrodacus_data(compressed_string)
+    pprint(uncompressed_string)
